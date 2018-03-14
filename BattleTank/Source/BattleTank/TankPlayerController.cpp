698adac4b2883314edd8eb26e5565fa762ab69bc
DESKTOP-UFRQFEF\seths <seth.shifrin@gmail.com>
2018-03-14 17:47:40 -0400

    Tick created

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,7 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -14,11 +15,26 @@ void ATankPlayerController::BeginPlay()
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank ->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName()));
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime) 
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+	
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	//Get world location if linetrace through crosshair
+	//if it hits the landscape
+		// Tell controlled tank to aim at this point
 }
